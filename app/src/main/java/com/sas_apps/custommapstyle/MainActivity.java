f4f16b5c8e85553f5878205e3b44880f4ae41ff1
Shashank Shinde <sha44.shinde@gmail.com>
2018-03-14 20:15:42 +0530

    More map styles

---
+++ app/src/main/java/com/sas_apps/custommapstyle/MainActivity.java
@@ -0,0 +1,49 @@
+package com.sas_apps.custommapstyle;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+        switch (view.getId()) {
+            case R.id.buttonAubergine:
+                intent.putExtra(getResources().getString(R.string.app_name), "Aubergine");
+                break;
+            case R.id.buttonDark:
+                intent.putExtra(getResources().getString(R.string.app_name), "Dark");
+                break;
+            case R.id.buttonSilver:
+                intent.putExtra(getResources().getString(R.string.app_name), "Silver");
+                break;
+            case R.id.buttonRetro:
+                intent.putExtra(getResources().getString(R.string.app_name), "Retro");
+                break;
+            case R.id.buttonNight:
+                intent.putExtra(getResources().getString(R.string.app_name), "Night");
+                break;
+            case R.id.buttonBlueWater:
+                intent.putExtra(getResources().getString(R.string.app_name), "BlueWater");
+                break;
+            case R.id.buttonDesert:
+                intent.putExtra(getResources().getString(R.string.app_name), "Desert");
+                break;
+            case R.id.buttonBrowns:
+                intent.putExtra(getResources().getString(R.string.app_name), "Browns");
+                break;
+            case R.id.buttonAvocado:
+                intent.putExtra(getResources().getString(R.string.app_name), "Avocado");
+                break;
+        }
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/sas_apps/custommapstyle/MapsActivity.java
+++ app/src/main/java/com/sas_apps/custommapstyle/MapsActivity.java
@@ -17,12 +17,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private static final String TAG = "MapsActivity";
+    private String mapStyle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        mapStyle=getIntent().getStringExtra(getResources().getString(R.string.app_name));
+        Log.d(TAG, "onCreate: getIntent "+mapStyle);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -30,21 +32,126 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+       switch (mapStyle){
+           case "Silver":
+               try {
+                   boolean success = googleMap.setMapStyle(
+                           MapStyleOptions.loadRawResourceStyle(
+                                   this, R.raw.silver));
+                   if (!success) {
+                       Log.e(TAG, "Style parsing failed.");
+                   }
+                    break;
+               } catch (Resources.NotFoundException e) {
+                   Log.e(TAG, "Can't find style. Error: ", e);
+                   break;
+               }
+           case "Retro":
+               try {
+                   boolean success = googleMap.setMapStyle(
+                           MapStyleOptions.loadRawResourceStyle(
+                                   this, R.raw.retro));
+                   if (!success) {
+                       Log.e(TAG, "Style parsing failed.");
+                   }
+                   break;
+               } catch (Resources.NotFoundException e) {
+                   Log.e(TAG, "Can't find style. Error: ", e);
+                   break;
+               }
+           case "Dark":
+               try {
+                   boolean success = googleMap.setMapStyle(
+                           MapStyleOptions.loadRawResourceStyle(
+                                   this, R.raw.dark));
+                   if (!success) {
+                       Log.e(TAG, "Style parsing failed.");
+                   }
+                   break;
+               } catch (Resources.NotFoundException e) {
+                   Log.e(TAG, "Can't find style. Error: ", e);
+                   break;
+               }
+           case "Night":
+               try {
+                   boolean success = googleMap.setMapStyle(
+                           MapStyleOptions.loadRawResourceStyle(
+                                   this, R.raw.night));
+                   if (!success) {
+                       Log.e(TAG, "Style parsing failed.");
+                   }
+                   break;
+               } catch (Resources.NotFoundException e) {
+                   Log.e(TAG, "Can't find style. Error: ", e);
+                   break;
+               }
+           case "Aubergine":
+               try {
+                   boolean success = googleMap.setMapStyle(
+                           MapStyleOptions.loadRawResourceStyle(
+                                   this, R.raw.aubergine));
+                   if (!success) {
+                       Log.e(TAG, "Style parsing failed.");
+                   }
+                   break;
+               } catch (Resources.NotFoundException e) {
+                   Log.e(TAG, "Can't find style. Error: ", e);
+                   break;
+               }
 
-
-        try {
-            // Customise the styling of the base map using a JSON object defined
-            // in a raw resource file.
-            boolean success = googleMap.setMapStyle(
-                    MapStyleOptions.loadRawResourceStyle(
-                            this, R.raw.map_style));
-
-            if (!success) {
-                Log.e(TAG, "Style parsing failed.");
-            }
-        } catch (Resources.NotFoundException e) {
-            Log.e(TAG, "Can't find style. Error: ", e);
-        }
+           case "Desert":
+               try {
+                   boolean success = googleMap.setMapStyle(
+                           MapStyleOptions.loadRawResourceStyle(
+                                   this, R.raw.desert));
+                   if (!success) {
+                       Log.e(TAG, "Style parsing failed.");
+                   }
+                   break;
+               } catch (Resources.NotFoundException e) {
+                   Log.e(TAG, "Can't find style. Error: ", e);
+                   break;
+               }
+           case "BlueWater":
+               try {
+                   boolean success = googleMap.setMapStyle(
+                           MapStyleOptions.loadRawResourceStyle(
+                                   this, R.raw.blue_water));
+                   if (!success) {
+                       Log.e(TAG, "Style parsing failed.");
+                   }
+                   break;
+               } catch (Resources.NotFoundException e) {
+                   Log.e(TAG, "Can't find style. Error: ", e);
+                   break;
+               }
+           case "Avocado":
+               try {
+                   boolean success = googleMap.setMapStyle(
+                           MapStyleOptions.loadRawResourceStyle(
+                                   this, R.raw.avocado));
+                   if (!success) {
+                       Log.e(TAG, "Style parsing failed.");
+                   }
+                   break;
+               } catch (Resources.NotFoundException e) {
+                   Log.e(TAG, "Can't find style. Error: ", e);
+                   break;
+               }
+           default:
+               try {
+                   boolean success = googleMap.setMapStyle(
+                           MapStyleOptions.loadRawResourceStyle(
+                                   this, R.raw.standerd));
+                   if (!success) {
+                       Log.e(TAG, "Style parsing failed.");
+                   }
+                   break;
+               } catch (Resources.NotFoundException e) {
+                   Log.e(TAG, "Can't find style. Error: ", e);
+                   break;
+               }
+       }
         mMap = googleMap;
         LatLng india = new LatLng(20.59, 78.96);
         mMap.addMarker(new MarkerOptions().position(india).title("India"));
